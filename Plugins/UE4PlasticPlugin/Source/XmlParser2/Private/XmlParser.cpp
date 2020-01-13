6be5b3d158aba77b93ebeab92bafaa6cd6159770
Jordan <jordanz@live.ca>
2020-01-13 13:30:10 -0700

    Uploading files

---
+++ Plugins/UE4PlasticPlugin/Source/XmlParser2/Private/XmlParser.cpp
@@ -0,0 +1,8 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "XmlParserPrivatePCH.h"
+
+#include "XmlParser.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE( FDefaultModuleImpl, XmlParser2 );
